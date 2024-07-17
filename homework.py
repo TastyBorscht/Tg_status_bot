3306a4bbcc03036f02ccf709d9a3244dc9a1eba0
Oleg Bryukhanov <olegbrukhanov@yandex.ru>
2024-07-17 17:11:27 +0300

    check env

--- homework.py
+++ homework.py
@@ -1,17 +1,17 @@
-...
+import os
+
+import requests
+from dotenv import load_dotenv
+from telebot import TeleBot, types
 
 load_dotenv()
 
-
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
-
-RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+RETRY_PERIOD = 600
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-
-
 HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
@@ -20,7 +20,15 @@ HOMEWORK_VERDICTS = {
 
 
 def check_tokens():
-    ...
+    """Проверка переменных среды."""
+    required_tokens = [
+        'PRACTICUM_TOKEN',
+        'TELEGRAM_TOKEN',
+        'TELEGRAM_CHAT_ID',
+    ]
+    for token in required_tokens:
+        if token not in os.environ:
+            raise ValueError(f'для работы бота не хватает токена {token}')
 
 
 def send_message(bot, message):
@@ -35,32 +43,31 @@ def check_response(response):
     ...
 
 
-def parse_status(homework):
-    ...
-
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+# def parse_status(homework):
+#     ...
+#
+#     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def main():
     """Основная логика работы бота."""
 
-    ...
+    check_tokens()
 
-    # Создаем объект класса бота
-    bot = ...
-    timestamp = int(time.time())
+    bot = TeleBot(token=TELEGRAM_TOKEN)
+    # timestamp = int(time.time())
 
-    ...
+    # ...
 
-    while True:
-        try:
-
-            ...
-
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
-        ...
+    # while True:
+    #     try:
+    #
+    #         ...
+    #
+    #     except Exception as error:
+    #         message = f'Сбой в работе программы: {error}'
+    #         ...
+    #     ...
 
 
 if __name__ == '__main__':
